c34b155ff8205582b5c374145a7d3596e931afb7
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-05 10:45:16 -0300

    learned about escope and use strict

---
+++ script.js
@@ -0,0 +1,18 @@
+/*var x = 7;
+console.log(x);*/
+
+/*function imprimir(){
+    x = 7;
+    console.log(x);
+};*/
+"use strict";
+
+function imprimir(){
+    //var x = 7;
+    x = 7;
+    console.log(x);
+};
+
+imprimir();
+console.log(x)
+console.log(window)
