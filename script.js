6f38fbc19f7c5d2d0e92117cdaa7fdd455966cfa
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-10 10:57:02 -0300

    learned map

--- script.js
+++ script.js
@@ -9,34 +9,12 @@ let students = [
     newStudent("Doly", 12),
     newStudent("Thor", 5),
 ];
-console.log(students)
 
-// for (let student of students){
-//     if (student.age >= 15){
-//         console.log(student.name)
-//     }
-// }
-
-function over15(students){
-    return students.age >= 15;
+function nameAndAge(student){
+    return student.name + " is " + student.age + " years old";
 }
 
-let studentsOver15 = students.filter(over15);
-console.log(studentsOver15);
-// console.log(students.filter(over15));
+console.log(students.map(nameAndAge));
 
-function filtro(callback){
 
-    let alunosFiltrados = [];
-    for (let aluno of this){
-        if (callback(aluno)){
-            alunosFiltrados.push(aluno)
-        }
-    }
-
-    return alunosFiltrados;
-}
-
-students.filtro = filtro;
-
-console.log(students.filtro(over15))
+console.log(students);
