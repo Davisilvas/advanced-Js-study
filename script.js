33f1a0815af627989972a8e598c6ae716565d55d
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-11 10:32:06 -0300

    first api, learned fetch

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-function novoAluno(nome, idade){
-    return {nome, idade}
+let url ="https://economia.awesomeapi.com.br/last/USD-BRL";
+
+fetch(url)
+    .then((res)=>{
+
+        return  res.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        // console.log(data.USDBRL.name)
+    })
+
+function converter(){
+    let input = document.getElementById("valor");
+    let valor = input.value;
+
+    fetch(url)
+    .then((res)=>{
+
+        return  res.json()
+    })
+    .then((data)=>{
+        let rate = data.USDBRL.low;
+        let resultado = valor + " dolares " + " equivalem a " + rate * valor + " reais."
+        document.getElementById("resultado").innerHTML = resultado
+        
+    })
 }
-
-let alunos = [
-    novoAluno("Davi", 21),
-    novoAluno("Maria", 23),
-    novoAluno("Doly", 12),
-    novoAluno("Thor", 5)
-]
-
-function somaIdade(total, aluno){
-    return total + aluno.idade; 
-}
-
-console.log(alunos.reduce(somaIdade, 0))
